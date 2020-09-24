ce81e48d942bb478a28f15b00978bfdb247688c9
Logan Garay <garay.logan@gmail.com>
2020-09-24 13:59:30 -0600

    connect MongoDB/created Bootcamp model

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const connectDB = require('./config/dbConfig');
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
 
 // Initial server setup
 const app = express();
+
+// Connect to database
+connectDB();
 
 // Middleware methods
 // NOTE these first two middleware methods are examples of a custom built logger and a third party logger; they perform the same job.
@@ -23,6 +27,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 // Starting up server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err}`);
+  // close server and exit process
+  server.close(() => process.exit(1));
+});
