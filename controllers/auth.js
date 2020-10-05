ede3d165cd45d6b90951617edff9f1115a9dfa4a
Logan Garay <garay.logan@gmail.com>
2020-10-05 15:37:09 -0600

    user can login

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,5 +14,32 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ success: true });
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+// POST login user FROM /api/v1/auth/login PUBLIC
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  // Validate email & password
+  if (!email || !password) {
+    return next(
+      new ErrorResponse('Please provide valid email or password', 400)
+    );
+  }
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+  // Create token
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, token });
 });
