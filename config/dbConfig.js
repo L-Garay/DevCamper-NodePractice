e5565454f1319b917a024c68f98b9824b9a90b71
Logan Garay <garay.logan@gmail.com>
2020-10-18 17:37:07 -0600

    added try/catch to dbConfig file

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const connection = await mongoose.connect(process.env.CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
-  console.log(`MongoDB connected: ${connection.connection.host}`);
+  try {
+    const connection = await mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.log(`ERROR: ${error}`);
+  }
 };
 module.exports = connectDB;
